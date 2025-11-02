6c1cafd71ded36cff0bc8b51f7778fc98ea19439
Maks0m0s <maksym.kozlovskyi150210@gmail.com>
2025-11-02 14:11:13 +0100

    CookieWithToken authentication improved

--- shop/api/home_views.py
+++ shop/api/home_views.py
@@ -1,6 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from django.shortcuts import render
 
 class HomeViewSet(viewsets.ViewSet):
+    permission_classes = [permissions.AllowAny]
+
     def list(self, request):
         return render(request, 'shop/home.html', {})
--- shop/authentication.py
+++ shop/authentication.py
@@ -1,12 +1,17 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.exceptions import AuthenticationFailed
 
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
-        # Get token from cookies instead of Authorization header
-        raw_token = request.COOKIES.get('access_token')
+        access_token = request.COOKIES.get('access_token')
 
-        if not raw_token:
+        if not access_token:
+            # 👇 Important: skip auth gracefully instead of failing
             return None
 
-        validated_token = self.get_validated_token(raw_token)
-        return self.get_user(validated_token), validated_token
+        try:
+            validated_token = self.get_validated_token(access_token)
+            return self.get_user(validated_token), validated_token
+        except Exception:
+            # 👇 Don’t crash; just let it act as an anonymous request
+            return None
