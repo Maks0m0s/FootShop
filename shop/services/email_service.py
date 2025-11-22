0c130f1201a28edbd9bd5cfb426a04e513c34fc8
Maks0m0s <maksym.kozlovskyi150210@gmail.com>
2025-11-22 18:00:12 +0100

    Revert "email_service.py modification"
    This reverts commit 6034b8624dde60d3567357e5ba9baa0cbc778e58.

--- shop/services/email_service.py
+++ shop/services/email_service.py
@@ -1,6 +1,5 @@
 from django.core.mail import send_mail
 from django.conf import settings
-from myproject.settings import EMAIL_HOST_USER
 
 from shop.models import OrderItem
 
@@ -31,9 +30,6 @@ def send_registration_email(user):
 def send_order_email(order):
     items = OrderItem.objects.filter(order=order)
 
-    #--- For Client ---
-
-
     subject = f"Your Order was placed!"
     message = f"""
     Hi {order.user.first_name or order.user.username},
@@ -61,41 +57,4 @@ def send_order_email(order):
         from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=recipient,
         fail_silently=False,
-    )
-
-    #--- For Admin ---
-
-    subject2 = f"NEW ORDER ({len(items)} item/s)"
-    items_list = ""
-    for item in items:
-        items_list+=f'- {item.product_name} | {item.chosen_size} | {item.player} - {item.number} | {item.product_price}€\n'
-    message2 = f"""
-    New order was placed by :
-    {order.user.first_name or order.user.username}
-    
-    -- Client Contact --
-    Email : {order.user.email}
-    
-    -- Order --
-    {items_list}
-    
-    -- Order Details --
-    Creating Date : {order.created_at.date()}
-    Aprox. Arriving Date : {order.arriving_date.date()}
-    Total Items : {items.count()}
-    Total Price : {order.total_price}€
-    
-    -- Delivery Details --
-    Country : {order.destination.country}
-    Address : {order.destination.address}
-    Postal Code : {order.destination.postal_code}
-    """
-    recipient2 = [EMAIL_HOST_USER]
-
-    send_mail(
-        subject=subject2,
-        message=message2,
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=recipient2,
-        fail_silently=True,
     )
